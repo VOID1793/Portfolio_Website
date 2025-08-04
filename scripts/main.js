edf5e0b26f535c808fb6fbc66a75c392b88885f9
Tyler Sell <tylersell93@gmail.com>
2025-08-04 01:00:39 -0400

    Additional styling, added required video, added audio.

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,28 @@
 /* This is my main JavaScript File. There isn't much here and it is quite basic.*/
+if (window.location.pathname.includes('resume.html')) {
+    console.log("We are testing the functionality of a JavaScript! This is a personal portfolio website.")
 
-console.log("We are testing the functionality of a JavaScript! This is a personal portfolio website.")
+    const datespan = document.getElementById('date-time');
+    const time = new Date().toLocaleString();
+    const currentcontent = datespan.textContent
 
-const datespan = document.getElementById('date-time');
-const time = new Date().toLocaleString();
-const currentcontent = datespan.textContent
+    console.log(datespan)
+    console.log(time)
+    console.log(currentcontent)
 
-console.log(datespan)
-console.log(time)
-console.log(currentcontent)
+    const newcontent = currentcontent.replace('$DATE', time)
 
-const newcontent = currentcontent.replace('$DATE', time)
-
-datespan.textContent = newcontent
+    datespan.textContent = newcontent
+}
+/* This script will play the background music when somebody presses the music button. */
+else if (window.location.pathname.includes('career-goals.html')) {
+    document.getElementById('play-button').addEventListener('click', function() {
+        var music = document.getElementById('music')
+        if (music.paused) {
+            music.play();
+        }
+        else {
+            music.pause();
+        }
+    })
+}
